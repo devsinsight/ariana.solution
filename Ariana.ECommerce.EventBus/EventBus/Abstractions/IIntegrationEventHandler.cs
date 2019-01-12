815c16868f2b88fa061ef5fc4f267472ee6443a3
José Luis Olivares Rojas <olivares.rojas.jose@gmail.com>
2019-01-12 11:04:20 -0500

    Ariana ECommerce Event Bus

---
+++ Ariana.ECommerce.EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ariana.ECommerce.EventBus.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariana.ECommerce.EventBus.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
