7265608aba7f37cabf4b97ed0d8a57a6d8b5bbab
José Luis Olivares Rojas <olivares.rojas.jose@gmail.com>
2019-01-13 20:17:18 -0500

    Ariana ECommerce Restructure

--- Ariana.Solution/Services/Basket/Ariana.ECommerce.Basket.Api/Startup.cs
+++ Ariana.Solution/Services/Basket/Ariana.ECommerce.Basket.Api/Startup.cs
@@ -40,7 +40,6 @@ namespace Ariana.ECommerce.Basket.Api
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
